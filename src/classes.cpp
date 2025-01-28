a1c10a12683e0fa3960ad8f0ebb9b95513a92db7
Josh <josh.rose137@gmail.com>
2025-01-28 14:11:49 +0000

    Try to display using SDL

---
+++ src/classes.cpp
@@ -0,0 +1,194 @@
+/*
+ * classes.cpp
+ *
+ * The implementation of the classes such as particles and the world.
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <SDL3/SDL.h>
+
+#include "classes.h"
+
+/*
+ * G
+ *
+ * The gravitational constant
+ */
+const double G = 6.67430 * std::pow(10, 0);
+const double TIME_STEP = 0.001;
+
+
+/*
+ * Particle
+ *
+ * Initialise a particle
+ */
+Particle::Particle(double _mass, double _x, double _y)
+{
+    x = _x;
+    y = _y;
+    mass = _mass;
+    ax = 0;
+    ay = 0;
+    vx = 0;
+    vy = 0;
+}
+
+
+/*
+ * update
+ *
+ * Update the position and velocity of a particle over a given time period.
+ *
+ * Argument: dt
+ *    The time period over which to update the position.
+ */
+void Particle::update(double dt)
+{
+    // Update position
+    x += vx * dt + 0.5 * ax * dt * dt;
+    y += vy * dt + 0.5 * ay * dt * dt;
+
+    // Update velocity
+    vx += ax * dt;
+    vy += ay * dt;
+}
+
+
+/*
+ * add_particle
+ *
+ * Adds a particle to the world.
+ */
+void World:: add_particle (Particle *p)
+{
+    particles.push_back(p);
+}
+
+
+/*
+ * apply_gravity
+ *
+ * Apply gravity to each particle.
+ */
+void World:: apply_gravity (void)
+{
+    /*
+     * For each particle, its acceleration is modified by the mass of all
+     * the other particles in the world.
+     */
+    for (auto p : particles) {
+        for (auto p_other : particles) {
+            if (p != p_other) {
+                p->ax -= (G * p_other->mass) / std::pow((p->x - p_other->x), 2);
+                p->ay -= (G * p_other->mass) / std::pow((p->y - p_other->y), 2);
+            }
+        }
+    }
+}
+
+
+/*
+ * update_particles
+ *
+ * Increment the particle positions and velocities over a discrete time
+ * period.
+ *
+ * Argument: dt
+ *    The time period over which the particles should be updated.
+ */
+void World:: update_particles (double dt)
+{
+    for (auto p : particles) {
+        p->update(dt);
+    }
+}
+
+
+/*
+ * show_particles
+ *
+ * Display all the particles in the world
+ */
+void World::show_particles(void) const {
+    for (auto p : particles) {
+        std::cout << "Particle at (" << p->x << ", " << p->y << ")\n";
+    }
+    std::cout << "====================\n";
+}
+
+
+/*
+ * simulate
+ *
+ * Simulate the behaviour of the particles over a given time period.
+ *
+ * Argument: run_time
+ *    The time over which to run the simulation.
+ */
+int World::simulate (double run_time)
+{
+    double       time_elapsed = 0.0;
+	SDL_Surface *winSurface   = NULL; // Pointers to our surface
+	SDL_Window  *window       = NULL; // Pointers to our window
+    int          rv           = 0;
+
+	// Initialize SDL. SDL_Init will return -1 if it fails.
+	if (!SDL_Init(SDL_INIT_VIDEO)) {
+		std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
+		rv = 1;
+	} 
+
+    /*
+     * For each time step:
+     * 1. Calculate the force of gravity exerted on all the particles
+     *    and update their accelerations.
+     * 2. Then update their new positions and velocities after a small
+     *    discrete time step.
+     * 3. Render/save their positions.
+     *
+     * Repeat until we've run for the alloted time.
+     */
+
+    if (rv == 0) {
+        window = SDL_CreateWindow("my window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOW_OPENGL);
+        if (window == NULL) {
+            std::cout << "Error creating window: " << SDL_GetError() << std::endl;
+            rv = 1;
+        }
+    }
+
+    if (rv == 0) {
+        winSurface = SDL_GetWindowSurface(window);
+        if (winSurface == NULL) {
+            std::cout << "Error getting window surface: " << SDL_GetError() << std::endl;
+            rv = 1;
+        }
+    }
+
+    if (rv == 0) {
+        SDL_FillSurfaceRect(winSurface, NULL, 1);
+        SDL_UpdateWindowSurface(window);
+    }
+
+    while (rv == 0 && time_elapsed < run_time) {
+        apply_gravity();
+        update_particles(TIME_STEP);
+        time_elapsed += TIME_STEP;
+        show_particles();
+    }
+
+    SDL_Log("Simulation complete");
+
+    if (rv == 0) {
+        SDL_DestroyWindow(window);
+        window = NULL;
+        winSurface = NULL;
+    }
+
+    SDL_Quit();
+
+    return rv;
+}
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include "classes.h"
+
+
+int
+main ()
+{
+    double duration = 0.0;
+    World world;
+    Particle p1(1, 2, 3);
+    Particle p2(2, 10, -10);
+
+    std::cout << "How long shall this run for in seconds?";
+    std::cin >> duration;
+ 
+    world.add_particle(&p1);
+    world.add_particle(&p2);
+
+    return world.simulate(duration);
+}
